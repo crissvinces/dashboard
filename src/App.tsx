86c83c728cfca5327bbf9ed3cae638925491269e
crissvinces <cvinces@espol.edu.ec>
2024-06-19 14:31:50 -0500

    actualizacion

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+
+    <Grid container spacing={5}>
+	      <Grid xs={12} sm={4} md={3} lg={2}>1</Grid>
+	      <Grid xs={6} sm={4} md={3} lg={2}>2</Grid>
+	      <Grid xs={6} sm={4} md={3} lg={2}>3</Grid>
+	      <Grid xs={12} sm={4} md={3} lg={2}>4</Grid>
+	      <Grid xs={6} sm={4} md={6} lg={2}>5</Grid>
+	      <Grid xs={6} sm={4} md={6} lg={2}>6</Grid>
+	    </Grid>
+   /* <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +39,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </>*/
   )
 }
 
